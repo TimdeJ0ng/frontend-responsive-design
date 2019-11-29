d38df97c03c47d00af15877ca32b0d4b0146d124
TimdeJ0ng <33430651+TimdeJ0ng@users.noreply.github.com>
2019-11-29 10:47:45 +0100

    HTML basis
    HTML basis

---
+++ java.js
@@ -0,0 +1,7 @@
+var zoekKnop = document.querySelector('#search');
+
+//var zoekVeld = document.querySelector('aside > form');
+
+zoekKnop.addEventListener('click', function(){
+  zoekKnop.classList.toggle('show-zoek');
+});
